23e5c279cf0b5990da25f73cd472dbe073247d8a
Janez K <janez.kranjc@gmail.com>
2017-05-04 13:46:40 +0200

    settings no longer required

--- cf_clus/library.py
+++ cf_clus/library.py
@@ -2,7 +2,6 @@ from subprocess import check_output, CalledProcessError
 from tempfile import NamedTemporaryFile
 import os
 import ConfigParser
-import settings
 
 
 def clus(input_dict):
@@ -11,13 +10,18 @@ def clus(input_dict):
     temporary_arff.write(input_dict['arff'])
     temporary_arff.close()
     temporary_settings = NamedTemporaryFile(suffix='.s', delete=False)
-    temporary_settings.write(input_dict['settings'])
+    settings = input_dict['settings']
+    if settings is None:
+        settings = ''
+    temporary_settings.write(settings)
     temporary_settings.close()
 
     # We need to change the filenames in the settings.
     settings = ConfigParser.RawConfigParser()
     settings.optionxform = str
     settings.read(temporary_settings.name)
+    if not settings.has_section('Data'):
+        settings.add_section('Data')
     settings.set('Data', 'File', temporary_arff.name)
     temporary_settings = open(temporary_settings.name, mode='wb')
     settings.write(temporary_settings)
@@ -34,13 +38,14 @@ def clus(input_dict):
     except CalledProcessError, e:
         raise Exception("An error has occured while trying to execute CLUS. Output:" + str(e.output) + " Cmd:" + str(
             e.cmd) + " return code: " + str(e.returncode))
-
-    output_file = open(temporary_settings.name.replace(".s", ".out"), 'rb')
-
-    output = output_file.read()
+    try:
+        output_file = open(temporary_settings.name.replace(".s", ".out"), 'rb')
+        output = output_file.read()
+        os.unlink(output_file.name)
+    except:
+        pass
 
     # We remove all temporary files.
     os.unlink(temporary_arff.name)
     os.unlink(temporary_settings.name)
-    os.unlink(output_file.name)
     return {'output': output}
