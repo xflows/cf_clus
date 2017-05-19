35bc0b14c5fd8a658ca6fc998d0db990cc67c1de
Janez K <janez.kranjc@gmail.com>
2017-05-19 12:52:22 +0200

    added setup.py and MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import os
+
+long_description = 'cf_clus is a ClowdFlows package for inducing Predictive Clustering Trees with CLUS.'
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+]
+
+dist = setup(
+    name='cf_clus',
+    version='1.0.0',
+    author='Janez Kranjc',
+    description='Induce predictive clustering trees in ClowdFlows.',
+    long_description=long_description,
+    author_email='janez.kranjc@gmail.com',
+    url='https://github.com/xflows/cf_clus',
+    license = 'GPL',
+    install_requires=['liac-arff',],
+    classifiers=CLASSIFIERS,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
