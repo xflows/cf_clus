4ee30e74acb1c8c6daef5bf0335a275fb6a45224
Janez K <janez.kranjc@gmail.com>
2017-05-23 14:26:41 +0200

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ CLASSIFIERS = [
 
 dist = setup(
     name='cf_clus',
-    version='1.0.2',
+    version='1.0.3',
     author='Janez Kranjc',
     description='Induce predictive clustering trees in ClowdFlows.',
     long_description=long_description,
